4221a5afd182dcc89a5b1aae994fabddcc779052
Ivan Ukhov <ivan.ukhov@gmail.com>
2019-06-23 15:22:20 +0200

    Read JSON configuration files

--- prediction/main.py
+++ prediction/main.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 from prediction.task import Task
 
@@ -7,4 +8,5 @@ if __name__ == '__main__':
     parser.add_argument('--action', type=str, required=True)
     parser.add_argument('--config', type=str, required=True)
     arguments = parser.parse_args()
-    Task().run(arguments.action, arguments.config)
+    with open(arguments.config) as file:
+        Task().run(arguments.action, json.load(file))
